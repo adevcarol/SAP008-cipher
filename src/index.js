ed557d30b58cf36ef7742213bbabf805f2debde1
adevcarol <alvscarolina@outlook.com>
2022-07-18 09:23:35 -0300

    cifragem de 1 letra com input de deslocamento

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import cipher from './cipher.js';
 
+// variáveis de entrada (retorno do HTML)
 const frm = document.querySelector("form")
-const outResp = document.querySelector("h3")
+const saidaNum = document.querySelector("h5")
+const saidaStr = document.querySelector("h3")
 
+// evento para quando o botão de cifrar for clicado
 frm.addEventListener("submit", (e) => {
-    const msg = frm.inMsg.value
-    const casas = Number(frm.inDes.value)
+    const mensagem = frm.inMsg.value // variável que está sendo atribuida a mensagem inserida pelo usuário no HTML
+    const desloc = Number(frm.inDesloc.value)
+    const index = 0
 
-    //cipher
+    // código para cifragem da palavra
+    // const cifra = mensagem.charCodeAt(index)
+    const asciiConvert = mensagem.charCodeAt(index)
 
-    outResp.innerText = msg
+    function cifra() {
+        return ((asciiConvert - 65) + desloc) % 26 + 65
+    }
+
+    // código para a palavra cifrada
+
+    saidaNum.innerText = cifra() // saida de texto com a resposta
+    saidaStr.innerText = String.fromCharCode(cifra()) // comando para converter novamente para string
 
     e.preventDefault()
 })
-
-// console.log(cipher)
