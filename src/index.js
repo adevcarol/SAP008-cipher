e2b76fc7e77038cc80e5a1d5d3e418694da28ac1
adevcarol <alvscarolina@outlook.com>
2022-07-20 18:33:31 -0300

    melhorias no html/css e adicao de animacoes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import cipher from './cipher.js';
 
 // variáveis de entrada (retorno do HTML)
-const frm = document.querySelector("form")
+const frm = document.querySelector("#cifrar")
 const saidaNum = document.querySelector("h5")
 const saidaStr = document.querySelector("h3")
 
 // evento para quando o botão de cifrar for clicado
 frm.addEventListener("submit", (e) => {
-    const mensagem = frm.inMsg.value // variável que está sendo atribuida a mensagem inserida pelo usuário no HTML
+    const mensagem = frm.inMsg.value.toUpperCase() // variável que está sendo atribuida a mensagem inserida pelo usuário no HTML
     const desloc = Number(frm.inDesloc.value)
     const index = 0
 
