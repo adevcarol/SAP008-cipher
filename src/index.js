f0ae18331b43880207f52afe4ba0ee1aa236142b
adevcarol <alvscarolina@outlook.com>
2022-07-24 23:06:38 -0300

    ajustes no codigo index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 import cipher from './cipher.js';
 
 // variáveis de entrada (retorno do HTML)
-const frm = document.querySelector("#cifrar")
-const saidaNum = document.querySelector("h5")
-const saidaStr = document.querySelector("h3")
+const frmCifrar = document.querySelector("#cifrar")
+const saidaNumC = document.querySelector("#h5cifrar")
+const saidaStrC = document.querySelector("#h3cifrar")
 
 // evento para quando o botão de cifrar for clicado
-frm.addEventListener("submit", (e) => {
-    const mensagem = frm.inMsg.value.toUpperCase() // variável que está sendo atribuida a mensagem inserida pelo usuário no HTML
-    const desloc = Number(frm.inDesloc.value)
+frmCifrar.addEventListener("submit", (e) => {
+    const mensagem = frmCifrar.inMsg.value.toUpperCase() // variável que está sendo atribuida a mensagem inserida pelo usuário no HTML
+    const desloc = Number(frmCifrar.inDesloc.value)
     const index = 0
 
     // código para cifragem da palavra
     // const cifra = mensagem.charCodeAt(index)
     const asciiConvert = mensagem.charCodeAt(index)
 
-    function cifra() {
-        return ((asciiConvert - 65) + desloc) % 26 + 65
+    function encode() {
+        return ((asciiConvert - 65) + desloc) % 26 + 65;
     }
 
     // código para a palavra cifrada
 
-    saidaNum.innerText = cifra() // saida de texto com a resposta
-    saidaStr.innerText = String.fromCharCode(cifra()) // comando para converter novamente para string
+    saidaNumC.innerText = encode() // saida de texto com a resposta
+    saidaStrC.innerText = String.fromCharCode(encode()) // comando para converter novamente para string
 
     e.preventDefault()
 })
